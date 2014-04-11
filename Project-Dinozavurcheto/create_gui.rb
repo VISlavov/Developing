627c40bcc1599fe422f5ff5fc35a27c30272c6a9
victor175 <victor.slavov.175@gmail.com>
2014-04-11 23:48:45 +0300

    1/2 from 2

--- Project-Dinozavurcheto/create_gui.rb
+++ Project-Dinozavurcheto/create_gui.rb
@@ -2,48 +2,13 @@ module CreateGui
   def create_gui renderer, music, screen
     app = App.new :renderer => renderer
 	
-		@change_view = 0
+	@change_view = 0
+
+	@button = Button.new "Start/Continue game", :x=>70, :y=>80, :w=>200, :h=>100, :padding_left=>20, :padding_top=>20
 	
-		@button = Button.new "Start/Continue game", :x=>70, :y=>80, :w=>200, :h=>100, :padding_left=>20, :padding_top=>20
-		
-		@button.on :pressed do |*opts|
-			@change_view = 1
-		end
-
-		modal_button = Button.new "Help", :x=>70, :y=>160, :padding_left=>20, :padding_top=>20
-		modal_button.on :pressed do |*opts|
-			modal = Dialog.new :modal => app, :x=>400, :y=>110, :w=>350, :h=>250
-
-			modal.add Label.new("Controls: ", :x=>20, :y=>10, :padding_left=>10, :padding_top=>10, :relative=>true)
-			modal.add TextField.new("W - up", :x=>20, :y=>70, :padding_left=>10, :padding_top=>10, :relative=>true)
-			modal.add TextField.new("S - down", :x=>20, :y=>100, :padding_left=>10, :padding_top=>10, :relative=>true)
-			modal.add TextField.new("A - left", :x=>20, :y=>130, :padding_left=>10, :padding_top=>10, :relative=>true)
-			modal.add TextField.new("D - right", :x=>20, :y=>160, :padding_left=>10, :padding_top=>10, :relative=>true)
-			
-			ok_butt = Button.new("OK", :x=>180, :y=>180, :padding_left=>20, :padding_top=>20,:relative=>true)
-			ok_butt.on :pressed do |*opts|
-				modal.close
-			end
-			
-			modal.add ok_butt
-			
-			modal.display
-		end
-		
-		modal_button2 = Button.new "Story of the trippy dino", :x=>70, :y=>320, :padding_left=>20, :padding_top=>20
-		modal_button2.on :pressed do |*opts|
-			modal2 = Dialog.new :modal => app, :x=>400, :y=>110, :w=>350, :h=>250
-
-				
-			ok_butt2 = Button.new("OK", :x=>180, :y=>180, :padding_left=>20, :padding_top=>20,:relative=>true)
-			ok_butt2.on :pressed do |*opts|
-				modal2.close
-			end
-			
-			modal2.add ok_butt2
-			
-			modal2.display
-		end
+	@button.on :pressed do |*opts|
+		@change_view = 1
+	end
 
     grp1 = RadioGroup.new :x=>200, :y=>190, :padding_left=>20, :padding_top=>20, :w=> 170, :h=>100
 
@@ -56,37 +21,37 @@ module CreateGui
     
     @disable_second = 42
     
-		grp_radio_one.on :checked do
-			if @disable_second == 0	|| @disable_second == 42
-				music.tag = RTS::MENU
-				music.play(:repeats => 100000)
-				if @disable_second == 42
-					@disable_second = 0
-				else
-					@disable_second = 1
-				end
-			else
+	grp_radio_one.on :checked do
+		if @disable_second == 0	|| @disable_second == 42
+			music.tag = RTS::MENU
+			music.play(:repeats => 100000)
+			if @disable_second == 42
 				@disable_second = 0
-			end
-		end
-		
-		grp_radio_two.on :checked do
-			if @disable_second == 0	|| @disable_second == 42
-				music.tag = RTS::LOADING
-				music.play(:repeats => 100000)
-				if @disable_second == 42
-					@disable_second = 0
-				else
-					@disable_second = 1
-				end
 			else
-					@disable_second = 0
+				@disable_second = 1
 			end
+		else
+			@disable_second = 0
 		end
+	end
+	
+	grp_radio_two.on :checked do
+		if @disable_second == 0	|| @disable_second == 42
+			music.tag = RTS::LOADING
+			music.play(:repeats => 100000)
+			if @disable_second == 42
+				@disable_second = 0
+			else
+				@disable_second = 1
+			end
+		else
+				@disable_second = 0
+		end
+	end
+	
+	grp1.add grp_label1, grp_label2, grp_label3, grp_radio_one, grp_radio_two
 		
-		grp1.add grp_label1, grp_label2, grp_label3, grp_radio_one, grp_radio_two
-			
-		grp1.hide
+	grp1.hide
 
     hide_button = Button.new "Settings", :x=>70, :y=>240, :padding_left=>20, :padding_top=>20
     hide_button.on :pressed do |*opts|
@@ -96,6 +61,43 @@ module CreateGui
         grp1.show
       end
     end
+    
+    modal_button = Button.new "Help", :x=>70, :y=>160, :padding_left=>20, :padding_top=>20
+	modal_button.on :pressed do |*opts|
+		puts 1
+		
+		modal = Dialog.new :modal => app, :x=>400, :y=>110, :w=>350, :h=>250
+
+		modal.add Label.new("Controls: ", :x=>20, :y=>10, :padding_left=>10, :padding_top=>10, :relative=>true)
+		modal.add TextField.new("W - up", :x=>20, :y=>70, :padding_left=>10, :padding_top=>10, :relative=>true)
+		modal.add TextField.new("S - down", :x=>20, :y=>100, :padding_left=>10, :padding_top=>10, :relative=>true)
+		modal.add TextField.new("A - left", :x=>20, :y=>130, :padding_left=>10, :padding_top=>10, :relative=>true)
+		modal.add TextField.new("D - right", :x=>20, :y=>160, :padding_left=>10, :padding_top=>10, :relative=>true)
+		
+		ok_butt = Button.new("OK", :x=>180, :y=>180, :padding_left=>20, :padding_top=>20,:relative=>true)
+		ok_butt.on :pressed do |*opts|
+			modal.close
+		end
+		
+		modal.add ok_butt
+		
+		modal.display
+	end
+		
+	modal_button2 = Button.new "Story of the trippy dino", :x=>70, :y=>320, :padding_left=>20, :padding_top=>20
+	modal_button2.on :pressed do |*opts|
+		modal2 = Dialog.new :modal => app, :x=>400, :y=>110, :w=>350, :h=>250
+			
+		ok_butt2 = Button.new("OK", :x=>180, :y=>180, :padding_left=>20, :padding_top=>20,:relative=>true)
+		
+		ok_butt2.on :pressed do |*opts|
+			modal2.close
+		end
+		
+		modal2.add ok_butt2
+		
+		modal2.display
+	end
     
     app.add @button, modal_button, grp1, hide_button, modal_button2
 
--- Project-Dinozavurcheto/dinozavurcheto.rb
+++ Project-Dinozavurcheto/dinozavurcheto.rb
@@ -5,98 +5,305 @@ require 'rubygame'
 require 'rubygoo'
 require './create_gui.rb'
 require "rubytrackselector"
+Rubygame::TTF.setup
 
 include Rubygame
 include Rubygoo
 include CreateGui
 
 class Game
-  def initialize
-    @screen = Rubygame::Screen.new [880,480], 0, [Rubygame::HWSURFACE, Rubygame::DOUBLEBUF]
-    @screen.title = "Trippy dino"
-
-    @queue = Rubygame::EventQueue.new
-    @clock = Rubygame::Clock.new
-    @clock.target_framerate = 30
-    
-    @first_iteration = 0
-    @first_iteration_2 = 0
-    
-    # Background
-	case rand(3) + 1
-    when 1
-		@background = Surface.load "models/Backgrounds/Background_1.png"
-		#background_menu = Surface.load "models/Backgrounds/Background_1_dim.png"
-	when 2
-		@background = Surface.load "models/Backgrounds/background_2.png"
-		#background_menu = Surface.load "models/Backgrounds/Background_2_dim.png"
-	when 3
-		@background = Surface.load "models/Backgrounds/background_3.png"
-		#background_menu = Surface.load "models/Backgrounds/Background_3_dim.png"
-	end
-    
-    #Music
-	@rts = RTS::Rts.new
-    @rts.local << File.join(File.dirname(__FILE__), "music")
-    @rts.enable_local = true
-    
-    #UI
-	@factory = AdapterFactory.new
-	@render_adapter = @factory.renderer_for :rubygame, @screen
-	@app = create_gui(@render_adapter, @rts, @screen)
-	@app_adapter = @factory.app_for :rubygame, @app
-  end
-
-  def run
-    loop do
-      update
-      draw
-      @clock.tick
-    end
-  end
-
-  def update
-	@queue.each do |ev|
-		case ev
-			when Rubygame::QuitEvent
-				Rubygame.quit
-				exit
-				
-			when KeyDownEvent
-				case ev.key
-					when K_ESCAPE
-						@change_view = 0
-					end
+	def collision_check evil, good
+		if (good.curr_x + 30 < evil.curr_x + 30 && good.curr_x + 30 > evil.curr_x - 30) && 
+		(good.curr_y + 30< evil.curr_y + 30 && good.curr_y + 30 > evil.curr_y - 30)
+			1
+		else
+			0
+		end
+	end	
+	
+	class Text
+		def initialize x=100, y=100, text="Hello, World!", size=48
+			@font = Rubygame::TTF.new "fonts/verdana.ttf", size
+			@text = text
 		end
 		
-		@app_adapter.on_event ev
-	end
-	
-	@app_adapter.update @clock.tick
-	if @change_view == 1
-		@background.blit @screen, [0, 0]
-		if @first_iteration == 0 && @rts.tag != RTS::LOADING
-			@rts.tag = RTS::ACTION_SCENE
-			@rts.play(:repeats => 100000)
-			@first_iteration = 1
-		else
-			@first_iteration_2 = 0
+		def rerender_text
+			@width,@height = @font.size_text(@text)
+			@surface = @font.render(@text, true, [255, 255, 255])
 		end
-	else
-		@app_adapter.draw @render_adapter
-		if @first_iteration_2 == 0 && @rts.tag != RTS::LOADING
-			@rts.tag = RTS::MENU
-			@rts.play(:repeats => 100000)
-			@first_iteration_2 = 1
-		else
-			@first_iteration = 0
+		
+		def text string
+			@text = string
+			rerender_text
 		end
 	end
-  end
 
-  def draw
-	@screen.update
-  end
+	
+	class Enemy
+		attr_accessor :curr_x, :curr_y
+		include Sprites::Sprite
+		
+		def initialize type
+			@image1 = Surface.load 'models/Stuff/PacMan_1.png'
+			@image2 = Surface.load 'models/Stuff/PacMan_2.png'
+			
+			@image3 = Surface.load 'models/Stuff/Eye_1.png'
+			@image4 = Surface.load 'models/Stuff/Eye_2.png'
+			@image5 = Surface.load 'models/Stuff/Eye_3.png'
+			@image6 = Surface.load 'models/Stuff/Eye_4.png'
+			@image7 = Surface.load 'models/Stuff/Eye_5.png'
+			@image8 = Surface.load 'models/Stuff/Eye_6.png'
+			
+			if type == 'pac'
+				@image = @image1
+			elsif type == 'eye'
+				@image = @image3
+			end
+				
+			@curr_x = rand 1300
+			@curr_y = 0
+			@rect = @image.make_rect
+			@rect.center = [@curr_x, @curr_y]
+			@speed = 30
+		end
+
+		def move
+			case @image
+				when @image7
+					@image = @image3
+				when @image6
+					@image = @image7
+				when @image5
+					@image = @image6
+				when @image4
+					@image = @image5
+				when @image3
+					@image = @image4
+				when @image2
+					@image = @image1
+				when @image1
+					@image = @image2
+			end
+			
+			@rect.center = [@curr_x = @curr_x - @speed / 1.5, @curr_y = @curr_y + @speed]
+			if @curr_x < 0 && @curr_y > 1000
+				@curr_x = rand 800
+				@curr_y = 0
+			end
+		end
+	end
+
+	class Player
+		include Sprites::Sprite
+		attr_accessor :breaker, :curr_x, :curr_y
+		
+		def initialize
+			@image1 = Surface.load 'models/Stuff/Forward_1.png'
+			@image2 = Surface.load 'models/Stuff/Forward_2.png'
+			@image3 = Surface.load 'models/Stuff/Back_1.png'
+			@image4 = Surface.load 'models/Stuff/Back_2.png'
+			@image = @image1
+			
+			@breaker = 1
+			@curr_x = 74
+			@curr_y = 300
+			@rect = @image.make_rect
+			@rect.center = [@curr_x, @curr_y]
+			@speed = 30
+			@jump_height = 30
+		end
+
+		def change_frame mode
+			case mode
+				when 'd'
+					image1 = @image1
+					image2 = @image2
+				when 'a'
+					image1 = @image3
+					image2 = @image4
+			end
+			
+			case @image
+				when @image2, @image4
+					@image = image1
+				when @image1, @image3
+					@image = image2
+			end
+			
+			@rect.center = [@curr_x, @curr_y]
+		end
+		
+		def jump
+		
+		end
+		
+		def move dir
+			case dir
+				when 'w'
+					#@rect.center = [@curr_x, @curr_y = @curr_y - @jump_height]
+					#sleep 0.42
+					#@rect.center = [@curr_x, @curr_y = @curr_y + @jump_height]
+				when 'a'
+					if @curr_x < 440
+						@rect.center = [@curr_x = @curr_x - @speed, @curr_y]
+						change_frame dir
+					else
+						
+					end
+				when 'd'
+					if @curr_x < 440
+						@rect.center = [@curr_x = @curr_x + @speed, @curr_y]
+						change_frame dir
+					else
+						
+					end
+			end
+				
+		end
+	end
+
+	def initialize
+		@screen = Rubygame::Screen.new [880,480], 0, [Rubygame::HWSURFACE, Rubygame::DOUBLEBUF]
+		@screen.title = "Trippy dino"
+
+		@queue = Rubygame::EventQueue.new
+		@clock = Rubygame::Clock.new
+		@clock.target_framerate = 30
+
+		@first_iteration = 0
+		@first_iteration_2 = 0
+		
+		@dino = Player.new
+		@pac = Enemy.new 'pac'
+		@eye = Enemy.new 'eye'
+		@direction = 42
+		@mul_click = 0
+		
+		@text = Text.new
+
+		# Background
+		case rand(3) + 1
+			when 1
+				@background = Surface.load "models/Backgrounds/Background_1.png"
+				#background_menu = Surface.load "models/Backgrounds/Background_1_dim.png"
+			when 2
+				@background = Surface.load "models/Backgrounds/background_2.png"
+				#background_menu = Surface.load "models/Backgrounds/Background_2_dim.png"
+			when 3
+				@background = Surface.load "models/Backgrounds/background_3.png"
+				#background_menu = Surface.load "models/Backgrounds/Background_3_dim.png"
+		end
+
+		#Music
+		@rts = RTS::Rts.new
+		@rts.local << File.join(File.dirname(__FILE__), "music")
+		@rts.enable_local = true
+
+		#UI
+		@factory = AdapterFactory.new
+		@render_adapter = @factory.renderer_for :rubygame, @screen
+		@app = create_gui(@render_adapter, @rts, @screen)
+		@app_adapter = @factory.app_for :rubygame, @app
+	end
+
+	def run
+		loop do
+			update
+			draw
+			@clock.tick
+		end
+	end
+
+	def update
+		@queue.each do |ev|
+			case ev
+				when Rubygame::QuitEvent
+					Rubygame.quit
+					exit
+
+				when KeyDownEvent
+					case ev.key
+						when K_ESCAPE
+							@change_view = 0
+						when K_W
+							@dino.move 'w'
+						when K_A, K_D
+							if @direction != 42
+								@mul_click = 1
+							else
+								@mul_click = 0
+							end
+							
+							@dino.breaker = 0
+							
+							if K_A == ev.key
+								@direction = 'a'
+							elsif K_D == ev.key
+								@direction = 'd'
+							end
+					end
+				
+				when KeyUpEvent
+					case ev.key
+						when K_A, K_D
+							if @mul_click == 0
+								@dino.breaker = 1
+								@direction = 42
+							else
+								@mul_click = 0
+							end
+					end
+			end
+
+			@app_adapter.on_event ev
+		end
+
+		@app_adapter.update @clock.tick
+
+		if @change_view == 1
+			if @first_iteration == 0 && @rts.tag != RTS::LOADING
+				@rts.tag = RTS::ACTION_SCENE
+				@rts.play(:repeats => 100000)
+				@first_iteration = 1
+			else
+				@first_iteration_2 = 0
+			end
+			
+			if @dino.breaker == 0
+				@dino.move @direction
+			end
+			
+			@pac.move
+			@eye.move
+			
+			@background.blit @screen, [0, 0]
+			@dino.draw @screen
+			@pac.draw @screen
+			@eye.draw @screen
+			
+			puts ' ------------------------- '
+			puts collision_check @pac, @dino
+			puts ' ------ '
+			puts collision_check @eye, @dino
+			puts ' ------------------------- '
+			
+			@text.text rand(100).to_s
+		else
+			@app_adapter.draw @render_adapter
+				
+			if @first_iteration_2 == 0 && @rts.tag != RTS::LOADING
+				@rts.tag = RTS::MENU
+				@rts.play(:repeats => 100000)
+				@first_iteration_2 = 1
+			else
+				@first_iteration = 0
+			end
+		end
+	end
+
+	def draw
+		@screen.update
+	end
 end
 
 game = Game.new
