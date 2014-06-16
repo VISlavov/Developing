37aaff0229ce9be8f295673f89b53d5b5b310df6
Georgi-Kratchkov <georgi_kratchkov@abv.bg>
2014-06-16 16:02:26 +0300

    some code

---
+++ Bitwise-Test-Creator/source/linker.rb
@@ -0,0 +1,8 @@
+require './c-parser.rb'
+require './html-parser.rb'
+require './pdf-parser.rb'
+require './c-runner.rb'
+require './generator.rb'
+require './organizer.rb'
+
+fill_file ["ei boklik", "mrusen", "gaden"]
---
+++ Bitwise-Test-Creator/templates/template.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main()
+{
+ei boklik;
+mrusen;
+gaden;
+return 0;
+}
