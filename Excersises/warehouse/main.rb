72245e1c8f0a91d20063e4aa215268a9df025be8
victor175 <victor.slavov.175@gmail.com>
2017-01-09 15:09:23 +0000

    remove inconsistent code

--- Excersises/warehouse/main.rb
+++
@@ -1,8 +0,0 @@
-require './warehouse.rb'
-
-warehouse = Warehouse.new()
-
-routes = warehouse.get_routes()
-stock_type_count = warehouse.get_stock_type_count()
-
-puts "#{stock_type_count} #{routes}"
