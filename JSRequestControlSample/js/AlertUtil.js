7863fea10afc5d5304baf9630485a197d12c4179
victor175 <victor.slavov.175@gmail.com>
2017-01-10 22:42:52 +0200

    final touches on JSRequestControlSample

--- JSRequestControlSample/js/AlertUtil.js
+++ JSRequestControlSample/js/AlertUtil.js
@@ -1,10 +1,17 @@
-var AlertUtil = function () {
+var AlertUtil = function (mainService) {
 	this.alertInvalidData = function() {
 		this.showAlert("Your data is invalid. Plase enter numbers.");
 	};
 
 	this.alertNumberOutOfRange = function() {
-		this.showAlert("Please enter a positive number, below 1000. ( 0 < number < 1000 )");
+		this.showAlert(
+			"Please enter a positive number, below " 
+			+ mainService.MAX_COUNT_BOUNDARY 
+			+ ". ( "
+			+ mainService.MIN_COUNT_BOUNDARY
+			+ " < number < "
+			+ mainService.MAX_COUNT_BOUNDARY
+			+ ")");
 	};
 
 	this.showAlert = function(message) {
--- JSRequestControlSample/js/MainService.js
+++ JSRequestControlSample/js/MainService.js
@@ -1,4 +1,7 @@
 var MainService = function() {
+	this.MIN_COUNT_BOUNDARY = 1;
+	this.MAX_COUNT_BOUNDARY = 1000;
+
 	this.getDesiredDataCount = function() {
 		var desiredDataCount = $('input#count-field').val();
 		desiredDataCount = parseInt(desiredDataCount);
@@ -7,12 +10,13 @@ var MainService = function() {
 	};
 
 	this.isDataCountValid = function(count) {
-		var alertUtil = new AlertUtil();
+		var alertUtil = new AlertUtil(this);
 		var isValid = false;
 
 		if(isNaN(count)) {
 			alertUtil.alertInvalidData();
-		} else if(count < 1 || count > 1000) {
+		} else if(count < this.MIN_COUNT_BOUNDARY 
+							|| count > this.MAX_COUNT_BOUNDARY) {
 			alertUtil.alertNumberOutOfRange();
 		} else {
 			isValid = true;
